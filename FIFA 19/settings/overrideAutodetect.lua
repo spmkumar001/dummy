46b22b3a271385d7cfe4a47a511ece515e2142c6
unknown <spmkumar001@gmail.com>
2022-07-18 21:38:25 +0530

    second commit

---
+++ FIFA 19/settings/overrideAutodetect.lua
@@ -0,0 +1,8 @@
+FullscreenEnabled = 1
+ResolutionWidth = 1920
+ResolutionHeight = 1080
+AntiAliasingDeferred = 2
+OverallGraphicsQuality = 2
+VSyncEnabled = 1
+PresentInterval = 1
+WindowedBorderless = 0
